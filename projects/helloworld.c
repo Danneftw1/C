962691789373984c32d1b45efdfc37c24738412a
Daniel Nilsson <daniel.nilsson1@iths.se>
2025-02-15 00:59:22 +0100

    created helloworld.c and helloworld executable

---
+++ projects/helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // standard input library, allows us to use functions like printf
+
+int main() { // entry point of any C program, this is where the execution of the program begins
+    printf("Hello, World! \n");
+    return 0; // "return 0" means that the program ended successfully, "1" for failed
+}
