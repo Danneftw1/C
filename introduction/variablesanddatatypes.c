3c3d140648592923814f441a74fdfcca819c4788
Daniel <danne.lol97@gmail.com>
2025-02-20 23:43:08 +0100

    data types and variables

---
+++ introduction/variablesanddatatypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int number = 12;
+    char name[] = "daniel";
+    float decimal = 12.32;
+
+    printf("number %d", number);
+    printf("name %s", name);
+    printf("decimal %.2f", decimal);
+
+    return 0;
+}
